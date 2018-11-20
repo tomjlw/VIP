28e7b5e8560ad9c2371f8a459eb3f689e5875924
Liwen <tomjlw@titan.ece.rice.edu>
2018-11-20 08:39:10 -0600

    Cirular COnvolution to Normal one

--- src/filter.m
+++ src/filter.m
@@ -18,7 +18,7 @@ subplot(2,1,2); plot(n,z); title('signal with noise');
 
 % % IIR Butterworth LPF filter Design
 % order = 100;  % order of the filter
- Wc = 0.05;%2*pi*f/fs;     % normalized cut-off frequency (as of signal freq)
+Wc = 0.05;%2*pi*f/fs;     % normalized cut-off frequency (as of signal freq)
 % [b,a] = butter(order,Wc,'low'); % Nr and Dr coeff. of IIR butterworth filter
 % fvtool(b,a); % filter frequency response
 % % filter the signal
@@ -28,16 +28,21 @@ subplot(2,1,2); plot(n,z); title('signal with noise');
 
 % FIR Filter Design
 order = 64;
-b1= fir1(order, Wc);
-bls = firls(order, [0 Wc 0.14 1], [1 1 0 0]); % FIR low pass filter
-bpm = firpm(order, [0 Wc 0.14 1], [1 1 0 0]); % FIR low pass filter
-figure(3);freqz(b1,1,512); % frequency response of FIR LPF filter
+%b1= fir1(order, Wc);
+b = firls(order, [0 Wc 0.14 1], [1 1 0 0]); % FIR low pass filter
+% bpm = firpm(order, [0 Wc 0.14 1], [1 1 0 0]); % FIR low pass filter
+% figure(3);freqz(b1,1,512); % frequency response of FIR LPF filter
 hold on;
-freqz(bls,1,512); % frequency response of FIR LPF filter
-freqz(bpm,1,512); % frequency response of FIR LPF filter
+freqz(b,1,512); % frequency response of FIR LPF filter
+% freqz(bpm,1,512); % frequency response of FIR LPF filter
 
 % filter the signal
-x_f_fir = filter(bls,1,z);
+tic
+x_f_fir = filter(b,1,z);
+toc
 final = x_f_fir(order/2:end);
 figure(4);plot(z, '--');hold on; plot(final); title('Filtered Sinusoidal Signal');
+
+
+
 
